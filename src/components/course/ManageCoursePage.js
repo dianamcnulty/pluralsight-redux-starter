29994dffc17aeef99649bf3e1a7e017689c65822
Diana McNulty <diana.mcnulty@libertymutual.com>
2019-02-11 10:53:48 -0500

    adds feedback to 'save' button

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -10,7 +10,8 @@ class ManageCoursePage extends React.Component {
 
     this.state = {
       course: Object.assign({}, this.props.course),
-      errors: {}
+      errors: {},
+      saving: false
     };
     this.updateCourseState = this.updateCourseState.bind(this);
     this.saveCourse = this.saveCourse.bind(this);
@@ -30,11 +31,13 @@ class ManageCoursePage extends React.Component {
 
   saveCourse(event) {
     event.preventDefault();
+    this.setState({saving: true})
     this.props.actions.saveCourse(this.state.course)
       .then(() => this.redirect());
 
   }
   redirect() {
+    this.setState({ saving: false })
   this.context.router.push('/courses');
 }
 render() {
@@ -47,6 +50,7 @@ render() {
       onSave={this.saveCourse}
       course={this.state.course}
       errors={this.state.errors}
+      saving={this.state.saving}
     />
   );
 }
